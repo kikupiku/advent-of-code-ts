697dfbe06641d10a7b6e70651025e0e366e17680
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2021-03-10 09:40:04 +0100

    turn txt file into array of objects

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -1,8 +1,15 @@
 import fs from 'fs';
 
-const puzzleInput = fs.readFileSync(`${__dirname}/input.txt`).toString()
-const inputs = puzzleInput.split('\n').map(element => element.split(': ').map(el => [el]));
-
-const passwordDB: {} = Object.fromEntries(inputs);
+const puzzleInput = fs.readFileSync(`${__dirname}/input.txt`).toString().replace(/\:/g, '').replace(/-/g, ' ').split('\n').map(element => element.split(' '));
+const passwordDB: object[] = puzzleInput.map(element => {
+  return {
+    "min": element[0],
+    "max": element[1],
+    "letter": element[2],
+    "password": element[3],
+  }
+});
 
 console.log(passwordDB);
+
+
