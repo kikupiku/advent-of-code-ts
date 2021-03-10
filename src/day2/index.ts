ff3b53f289831527ed5d0b5018b7cc42c2f0b114
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2021-03-10 10:22:06 +0100

    plan out day2 functions

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -1,15 +1,32 @@
 import fs from 'fs';
 
-const puzzleInput = fs.readFileSync(`${__dirname}/input.txt`).toString().replace(/\:/g, '').replace(/-/g, ' ').split('\n').map(element => element.split(' '));
-const passwordDB: object[] = puzzleInput.map(element => {
+const puzzleInput = fs
+  .readFileSync(`${__dirname}/input.txt`)
+  .toString()
+  .replace(/:/g, '')
+  .replace(/-/g, ' ')
+  .split('\n')
+  .map((element) => element.split(' '));
+const passwordDB = puzzleInput.map((element) => {
   return {
-    "min": element[0],
-    "max": element[1],
-    "letter": element[2],
-    "password": element[3],
-  }
+    min: parseInt(element[0]),
+    max: parseInt(element[1]),
+    letter: element[2],
+    password: element[3],
+  };
 });
 
+const hasMinimum = (database: object): boolean => {
+
+  return true;
+};
+
+const isLessThanMaximum = (database: object): boolean => {
+  return true;
+};
+
+const howManyValidPasswords = (min: () => {}, max: () => {}): number => {
+  return 0;
+}
+
 console.log(passwordDB);
-
-
