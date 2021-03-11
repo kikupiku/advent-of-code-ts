a91322d6f0b7988c55c0399bde1be9178a3b8734
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2021-03-11 22:39:04 +0100

    day4 part1 wip

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -1,3 +1,24 @@
 import fs from 'fs';
 
-const passportDB = fs.readFileSync(`${__dirname}/input.txt`).toString();
+type ValidPassportCredentials = {
+  byr: string,
+  cid?: string,
+  ecl: string,
+  eyr: string,
+  hcl: string,
+  hgt: string,
+  iyr: string,
+  pid: string,
+}
+
+const passportDB: string[][][] = fs
+  .readFileSync(`${__dirname}/input.txt`)
+  .toString()
+  .trim()
+  .split('\n\n')
+  .map((passport) => {
+    const credentials = passport.replace(/\n/g, ' ').split(' ').map(credential => credential.split(':'));
+    return credentials;;
+  });
+
+console.log(passportDB);
