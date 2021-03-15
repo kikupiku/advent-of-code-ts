653832f8d6494d7736171a0a165e8cdc39a9bd21
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2021-03-15 19:08:10 +0100

    day4 part1 done

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -1,15 +1,16 @@
 import fs from 'fs';
 
-type ValidPassportCredentials = {
-  byr: string,
-  cid?: string,
-  ecl: string,
-  eyr: string,
-  hcl: string,
-  hgt: string,
-  iyr: string,
-  pid: string,
-}
+interface PassportCredentials {
+  byr?: string;
+  cid?: string;
+  ecl?: string;
+  eyr?: string;
+  hcl?: string;
+  hgt?: string;
+  iyr?: string;
+  pid?: string;
+  isValid?: boolean;
+};
 
 const passportDB: {}[] = fs
   .readFileSync(`${__dirname}/input.txt`)
@@ -25,11 +26,27 @@ const passportDB: {}[] = fs
   })
   .map((passport) => Object.fromEntries(passport));
 
-// const validPassports: number = passportDB.filter(passport => {
-//   let isValid: boolean = true;
-//   // for (key in passport) {
-//   }
+const checkPassports = (database: {}[]): {}[] => {
+  database.map((passport) => {
+    if (
+      passport.hasOwnProperty('byr') &&
+      passport.hasOwnProperty('ecl') &&
+      passport.hasOwnProperty('eyr') &&
+      passport.hasOwnProperty('hcl') &&
+      passport.hasOwnProperty('hgt') &&
+      passport.hasOwnProperty('iyr') &&
+      passport.hasOwnProperty('pid')
+    ) {
+      Object.assign(passport, { isValid: true });
+    } else {
+      Object.assign(passport, { isValid: false });
+    }
+  });
+  return database;
+};
 
-// });
+const howManyValidPassports = (database: PassportCredentials[]): number => {
+  return checkPassports(database).filter((passport: any) => passport.isValid).length;  //how to solve it without "any"?
+};
 
-console.log(validPassports);
+console.log(howManyValidPassports(passportDB));
